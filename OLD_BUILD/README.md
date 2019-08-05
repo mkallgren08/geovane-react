e14bd37f73d6076bef83441c1ddcb9e50a5fe13e
mkallgren08 <mkallgren08@gmail.com>
2019-08-05 14:01:22 -0400

    Started a new build, decided against it

---
+++ OLD_BUILD/README.md
@@ -0,0 +1,3 @@
+# GeoVane
+
+Welcome to the website for GeoVane's source code. GeoVane is an app that combines navigation planning with weather forecasting.
--- OLD_BUILD/routes/api/index.js
+++ OLD_BUILD/routes/api/index.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const testdataRoutes = require('./testdata.js');
+const testdataRoutes = require('./testdata.js.js');
 
 //  routes
 router.use("/testdata", testdataRoutes);
